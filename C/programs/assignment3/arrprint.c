482ddf4c8d5d4c0bb433b21c053ab235f7c5e5e9
sharanya1005 <sharanya100598@gmail.com>
2019-02-06 21:34:57 +0530

    program to print

---
+++ C/programs/assignment3/arrprint.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int x[8]={10,20,30,40,50,60,70,80};
+
+printf("x = %d\n",x);
+printf("x+2 =%d\n",x+2);
+printf("*x =%d\n",*x);
+printf("*x+2 =%d\n",*x+2);
+printf("*(x+2) =%d\n",*(x+2));
+}

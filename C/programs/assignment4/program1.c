28e668bbac1d5466767b7c7916abcbca09279071
sharanya1005 <sharanya100598@gmail.com>
2019-02-09 23:41:48 +0530

    Added first program

---
+++ C/programs/assignment4/program1.c
@@ -0,0 +1,19 @@
+﻿#include<stdio.h>
+int main()
+{
+float table[2][3] = {{1.1,1.2,1.3},{2.1,2.2,2.3}};
+printf("table = %d",table);
+printf("(table+1) = %d",(table+1));
+printf(" *(table+1) = %d",*(table+1) );
+printf(" (*(table+1)+1) = %d",(*(table+1)+1) );
+printf(" (*(table)+1) = %d",(*(table)+1) );
+printf(" *(*(table+1)+1) = %f",*(*(table+1)+1) );
+printf(" *(*(table)+1) = %f",*(*(table)+1) );
+printf(" (*(*table)+1) = %f",(*(*table)+1) );
+printf(" *(*(table+1)) = %f",*(*(table+1)) );
+printf(" (*(*table)+1)+1 = %f",(*(*table)+1)+1 );
+}
+
+
+
+ 

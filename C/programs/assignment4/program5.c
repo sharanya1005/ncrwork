a923a3e53e0bed700cc80d806f2239f881096a52
sharanya1005 <sharanya100598@gmail.com>
2019-02-09 23:45:11 +0530

    lower case string to upper case string program

---
+++ C/programs/assignment4/program5.c
@@ -0,0 +1,22 @@
+﻿#include<stdio.h>
+int main()
+{
+char s[50];
+gets(s);
+for(int i=0;s[i]!='\0';i++)
+{
+if(s[i]>='a' && s[i]<='z')
+{
+s[i]= (s[i]-32);
+}
+else 
+if(s[i] = ' ')
+{
+}
+else
+{
+s[i]=(s[i]+32);
+}
+}
+printf("%s\n",s);
+}

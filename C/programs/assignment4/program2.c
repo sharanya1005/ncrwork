1e353502d19ee55243c24f8f880c07918951d80d
sharanya1005 <sharanya100598@gmail.com>
2019-02-09 23:43:29 +0530

    this is the second program

---
+++ C/programs/assignment4/program2.c
@@ -0,0 +1,12 @@
+﻿#include<stdio.h>
+int main()
+{
+char *color[6] = {"red" , "green", "blue", "white", "black", "yellow" };
+printf(" color = %d\n ",color);
+printf(" (color+2) = %d\n ", (color+2) );
+printf(" *color = %d\n ", *color);
+printf(" *(color+2) = %d\n ", *(color+2) );
+printf(" color[5] = %d\n ", color[5] );
+printf(" *(color+5) = %d\n", *(color +5) );
+}
+

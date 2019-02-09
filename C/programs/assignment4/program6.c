135548c336f4553312709c8078356f4f741ab2db
sharanya1005 <sharanya100598@gmail.com>
2019-02-09 23:46:16 +0530

    reverse a string using recursive function

---
+++ C/programs/assignment4/program6.c
@@ -0,0 +1,30 @@
+﻿#include<stdio.h>
+int reverse(char[], int , int );
+int main()
+{
+char a[30];
+printf("Enter a string: ");
+gets(a);
+int l= strlen(a);
+reverse(a, 0, l-1);
+}
+int reverse(char b[], int x, int y)
+{
+int i = x;
+int j = y;
+if(i>=j)
+{
+printf("%s",b);
+return 0;
+}
+char temp;
+temp = b[i];
+b[i]= b[j];
+b[j]=temp;
+i++;
+j--;
+reverse(b,i,j);
+return 0;
+}
+
+

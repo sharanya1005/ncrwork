eefd7f2ce7f68fe8e2abfea09f2dfbef8e9da8ca
sharanya1005 <sharanya100598@gmail.com>
2019-02-09 23:44:19 +0530

    replace program

---
+++ C/programs/assignment4/program4.c
@@ -0,0 +1,25 @@
+﻿#include<stdio.h>
+int replace(char *);
+void main()
+{
+char *cat= "The cat sat";
+int n;
+n=replace(cat);
+printf(" number of spaces replaced: %d\n",n);
+}
+int replace(char *a)
+{
+int i=0, n=0;
+	for(i=0; a[i]!='\0'; i++)
+	{
+	if(a[i] == ' ')
+	{
+	 a[i] = '-';
+	n++;
+	}
+	}
+
+printf("new string is: %s\n", a);
+return n;
+}
+

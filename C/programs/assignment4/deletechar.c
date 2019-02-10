e7522c20e651acbca481b628dd081c262ff0a65b
sharanya1005 <sharanya100598@gmail.com>
2019-02-10 10:55:02 +0530

    few changes are made

---
+++ C/programs/assignment4/deletechar.c
@@ -0,0 +1,27 @@
+﻿#include<stdio.h>
+void main()
+{
+	char a[10],c;
+	int i, j;
+	printf("enter a string");
+	gets(a);
+	printf("\n enter a character");
+	scanf("%c",&c);
+	for (int k = 0; k < strlen(a); k++)
+	{
+		i = 0;
+		if (a[i] == c)
+		{
+			for (j = i + 1; j < strlen(a); j++)
+			{
+				a[i] = a[j];
+				i++;
+			}
+			a[i] = '\0';
+			
+		}
+		
+	}
+	printf("%s", a);
+
+}
---
+++ C/programs/assignment4/program3.c
@@ -0,0 +1,25 @@
+﻿#include<stdio.h>
+void main()
+{
+int count=0, overallcount=0;
+char *a;
+char* s [ ] = {"we will teach you how to ", "Move a mountain ", "Level a building " , "Erase the past ", "Make a million "};
+for(int j=0; j<5; j++)
+{
+	char *a[] = *(s+j);
+	for(int i=0; a[i]!='\0'; i++)
+	{
+		if(a[i] == 'e')
+			{
+			count++;
+			}
+	}
+overallcount = count + overallcount;
+printf(" a[%d] = %d\n",j,count);
+count=0;
+}
+printf("\n overall count of e is %d", overallcount);
+}
+
+
+

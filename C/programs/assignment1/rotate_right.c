9352fe5b32d2d4c1d97222a508f2de13ebf33f23
sharanya1005 <sharanya100598@gmail.com>
2019-02-10 13:10:59 +0530

    rotate right program

---
+++ C/programs/assignment1/rotate_right.c
@@ -0,0 +1,22 @@
+﻿#include<stdio.h>
+#include<math.h>
+int rotate_right(int, int);
+void main()
+{
+	int n = 0, b = 0,res=0;
+	printf("Enter the number: ");
+	scanf("%d", &n);
+	printf("Enter number of bits to be rotated: ");
+	scanf("%d", &b);
+	res = rotate_right(n, b);
+	printf("result =%d", res);
+}
+int rotate_right(int n, int b)
+{
+	int res,rem,quo,num1;
+	rem = n%100;
+	num1 = rem * pow(10,b);
+	quo = n/100;
+	res = num1 + quo;
+	return res;
+}

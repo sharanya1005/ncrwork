de1a9207964e0cf210777e7677f4c2902741647e
sharanya1005 <sharanya100598@gmail.com>
2019-02-10 10:46:20 +0530

    adding program of finding sub array

---
+++ C/programs/assignment3/program1.c
@@ -0,0 +1,38 @@
+﻿#include<stdio.h>
+void main()
+{
+	int len, i=0, j, sum,res;
+	printf("enter the size of array: ");
+	scanf("%d", &len);
+	int a[10];
+	printf("\n enter array elements: ");
+	int n = len;
+	for (int k = 0; k < n; k++)
+	{
+		scanf("%d", &a[k]);
+
+	}
+	printf("\n enter the sum: ");
+	scanf("%d", &sum);
+	for (i = 0;i < n; i++)
+	{
+		res = a[i];
+		for (j = i + 1; j < n; j++)
+		{
+			if (res == sum)
+			{
+				printf("Sum found between indexes %d and %d", i, j - 1);
+				break;
+			}
+			else if ((res > sum && j == n)||(sum==0&&res!=0))
+			{
+				printf("\n no sub array found");
+				break;
+			}
+			res = res + a[j];
+		}
+
+	}
+
+
+}

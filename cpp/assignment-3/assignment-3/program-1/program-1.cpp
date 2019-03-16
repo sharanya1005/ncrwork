00500a366e53ff99ce57e937afc2319c0c5ef0ed
sharanya1005 <sharanya100598@gmail.com>
2019-03-16 23:48:17 +0530

    assignment-3

---
+++ cpp/assignment-3/assignment-3/program-1/program-1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+template <class T>
+int linear_search(T array[], int size_arr, T element)
+{
+	int count, flag = 0;
+	for (count = 0; count < size_arr; count++)
+	{
+		if (array[count] == element)
+		{
+			flag = 1;
+			break;
+		}
+	}
+	return flag;
+}
+int linear_search(char *array[], int size_arr, char *element)
+{
+	int count, flag = 0;
+	for (count = 0; count < size_arr; count++)
+	{
+		if (strcpy(array[count], element) == 0)
+		{
+			flag = 1;
+			break;
+		}
+	}
+	return flag;
+}
+int main()
+{
+	int *arr_int, size_IntArr, size_str, result;
+	//char *arr_char;
+	char **arr_string;
+	cout << "enter the size of integer array\n";
+	cin >> size_IntArr;
+	arr_int = new int[size_IntArr];
+	cout << "enter the integer array elements\n";
+	for (int count = 0;count < size_IntArr;count++)
+
+}
